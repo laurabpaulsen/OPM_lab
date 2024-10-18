6834ab11670f6fd6384ba4a4de3f7126a8301e17
laurabpaulsen <202005791@post.au.dk>
2024-10-18 14:19:26 +0200

    add class for visualisation

--- OPM_lab/digitising.py
+++ OPM_lab/digitising.py
@@ -1,5 +1,3 @@
-
-import numpy as np
 import matplotlib.pyplot as plt
 import os
 import math
@@ -11,20 +9,26 @@ from fastrak_connector import FastrakConnector
 BASE_DIR = Path(__file__).resolve().parent
 SOUND_DIR = BASE_DIR / 'soundfiles'
 
-def idx_of_next_point(distance, idx, limit = 30):
-    wrong_beep_path = SOUND_DIR / 'wrongbeep.wav'
-    beep_path = SOUND_DIR / 'beep.wav'
 
+def play_sound(sound_type):
+    if sound_type == "beep":
+        os.system(f'afplay "{SOUND_DIR / "beep.wav"}"')
+    elif sound_type == "wrong":
+        os.system(f'afplay "{SOUND_DIR / "wrongbeep.wav"}"')
+    elif sound_type == "done":
+        os.system(f'afplay "{SOUND_DIR / "done.mp3"}"')
+
+
+def idx_of_next_point(distance, idx, limit = 30):
     # if stylus was clicked more than limit away from the head reference, the last point is undone
     if distance > limit:
-        # Play a beep sound to indicate that data is ready
-        os.system(f'afplay "{str(wrong_beep_path)}"')
+        play_sound("wrong")
         if idx == 0:
             return 0, False
         else:
             return idx - 1, False
     else: # moving on to the next
-        os.system(f'afplay "{str(beep_path)}"')
+        play_sound("beep")
         return idx + 1, True
 
 
@@ -35,57 +39,72 @@ def calculate_distance(point1, point2):
     
     return math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
 
-def update_message_box(ax, category, label, message):
-    """Updates the message box with current sensor information."""
-    ax.clear()
-    ax.axis('off')  # Turn off axis lines
-    ax.text(0.5, 0.4, f'{category}: {label}', va='center', ha='center', fontsize=14)
-    ax.text(0.5, 0.8, message, va='center', ha='center', fontsize=14, color='red')
-
-def add_point_3d(x, y, z, category, ax):
-    """Add a point to the 3D plot."""
-    alpha = 1
-    size = 6
-    if category == "OPM":
-        color = "blue"
-    elif category == "EEG":
-        color = "orange"
-    elif category == "fiducials":
-        color = "green"
-    else:
-        color = "k"
-        alpha = 0.2
-        size = 3
-
-    ax.scatter(x, y, z, c=color, label=category, alpha = alpha, s = size)
 
 
-def setup_figure(set_axes_limits = True):
-    # set up the figure with two subplots: one for 3D plot, one for messages
-    fig = plt.figure(figsize=(10, 6))
+
+class DigitisationPlotter:
+    def __init__(self, set_axes_limits=True):
+        self.fig, self.ax_3d, self.ax_text = self.setup_figure(set_axes_limits)
+
+    def setup_figure(self, set_axes_limits=True):
+        # Set up the figure with two subplots: one for 3D plot, one for messages
+        fig = plt.figure(figsize=(10, 6))
+
+        # 3D plot subplot
+        ax_3d = fig.add_subplot(121, projection='3d')
+        ax_3d.set_xlabel('X')
+        ax_3d.set_ylabel('Y')
+        ax_3d.set_zlabel('Z')
+        ax_3d.set_title('Points Digitised')
+        if set_axes_limits:
+            ax_3d.set_xlim([-30, 30])
+            ax_3d.set_ylim([-30, 30])
+            ax_3d.set_zlim([-30, 30])
+
+        # Textbox subplot
+        ax_text = fig.add_subplot(122)
+        ax_text.axis('off')
+
+        return fig, ax_3d, ax_text
+
+    def update_message_box(self, category, label, message):
+        """Updates the message box with the current sensor information."""
+        self.ax_text.clear()
+        self.ax_text.axis('off')  # Turn off axis lines
+        self.ax_text.text(0.5, 0.4, f'{category}: {label}', va='center', ha='center', fontsize=14)
+        self.ax_text.text(0.5, 0.8, message, va='center', ha='center', fontsize=14, color='red')
+
+    def add_point_3d(self, x, y, z, category):
+        """Add a point to the 3D plot."""
+        alpha = 1
+        size = 6
+        if category == "OPM":
+            color = "blue"
+        elif category == "EEG":
+            color = "orange"
+        elif category == "fiducials":
+            color = "green"
+        else:
+            color = "k"
+            alpha = 0.2
+            size = 3
+
+        self.ax_3d.scatter(x, y, z, c=color, label=category, alpha=alpha, s=size)
+
+    def refresh_plot(self):
+        """Refreshes the plot to reflect any new changes."""
+        plt.draw()
+        plt.pause(0.1)
     
-    # 3D plot subplot
-    ax_3d = fig.add_subplot(121, projection='3d')
-    ax_3d.set_xlabel('X')
-    ax_3d.set_ylabel('Y')
-    ax_3d.set_zlabel('Z')
-    ax_3d.set_title(f'Points digitised')
-    if set_axes_limits:
-        ax_3d.set_xlim([-30, 30])
-        ax_3d.set_ylim([-30, 30])
-        ax_3d.set_zlim([-30, 30])
-
-    # textbox subplot
-    ax_text = fig.add_subplot(122)
-    ax_text.axis('off')
-
-    return fig, ax_3d, ax_text
+    def close_plot(self):
+        """Closes the plot."""
+        plt.close()
 
 
 
 
 class Digitiser:
-    def __init__(self, connector: FastrakConnector):
+    def __init__(self, connector: FastrakConnector, plotter: DigitisationPlotter = None):
         """
         Args:
             connector (FastrakConnector):
@@ -93,6 +112,11 @@ class Digitiser:
         self.connector = connector
         self.digitised_points = pd.DataFrame(columns=['category','label','x','y','z'])
         self.digitisation_scheme = []
+
+        if plotter is None:
+            self.plotter = DigitisationPlotter()
+        else: 
+            self.plotter = plotter
         
     def add(self, category, labels = None, dig_type = "single", n_points = None):
         """
@@ -118,13 +142,32 @@ class Digitiser:
             "n_points": n_points
             })
             
+    def update_digitised_data(self, category, label, position):
+        """
+        Helper method to update the 3D plot and digitised points DataFrame.
+
+        Args:
+            category (str): The category of the point (e.g., OPM, EEG, fiducials).
+            label (str): The label of the point (e.g., specific marker name).
+            position (tuple): The (x, y, z) coordinates of the point.
+            ax_3d (Axes3DSubplot): The 3D axes object to update the plot.
+        """
+        # Add the point to the 3D plot
+        self.plotter.add_point_3d(*position, category=category)
+
+        new_data = pd.DataFrame({
+            "category": [category],
+            "label": [label],
+            "x": [position[0]],
+            "y": [position[1]],
+            "z": [position[2]]
+            })
+
+        self.digitised_points = pd.concat([self.digitised_points, new_data], ignore_index=True)
 
     def digitise_continuous(self, category, n_points, ax_3d, ax_text):
-        update_message_box(ax_text, label=category, category=category,
+        self.plotter.update_message_box(ax_text, label=category, category=category,
                         message="Ready for digitization... Press the stylus button to record points.") 
-        
-        plt.draw()  # Ensures the initial plot is drawn
-        plt.show(block=False)  # Show the plot without blocking the rest of the code
 
         idx = 0
         print("Press the stylus button to start scanning")
@@ -134,26 +177,13 @@ class Digitiser:
         while idx < n_points:
             data, position = self.connector.get_position_relative_to_head_receiver()
                 
-            self.digitised_points = pd.concat(
-                [self.digitised_points,
-                pd.DataFrame.from_dict({
-                    "category": category,
-                    "label": category,
-                    "x": position[0],
-                    "y": position[1],
-                    "z": position[2]
-                    })])
-                
-            print(f"Point {idx + 1}/{n_points} digitized at {position}")
+            self.update_digitised_data(category=category, label=category, position=position, ax_3d=ax_3d)
             idx += 1  # Move to the next point
-                    
-            # Update the 3D plot with the newly captured point
-            add_point_3d(*position, ax=ax_3d, category=category)
-            plt.draw()  # Refresh the plot
+            self.plotter.refresh_plot()
 
-        os.system(f'afplay "{SOUND_DIR / "done.mp3"}"')
+        play_sound("done")
 
-    def digitise_single(self, category, labels, ax_3d, ax_text, limit = 30):
+    def digitise_single(self, category, labels, limit = 30):
         """
  
         """
@@ -161,9 +191,8 @@ class Digitiser:
 
         idx = 0
         
-        update_message_box(ax_text, sensor_name=labels[idx], category=category, message="Ready for digitization...") 
-        plt.draw()  # Ensures the initial plot is drawn
-        plt.pause(0.1)
+        self.plotter.update_message_box(label=labels[idx], category=category, message="Ready for digitization...")
+        self.plotter.refresh_plot()
 
 
         while idx < len(labels):
@@ -177,37 +206,23 @@ class Digitiser:
             idx, cont = idx_of_next_point(calculate_distance(point1, point2), idx, limit=limit)
             
             if idx <= len(labels)-1:
-                update_message_box(ax_text, sensor_name=labels[idx], category=category, message="Now digitising:") 
+                self.plotter.update_message_box(label=labels[idx], category=category, message="Now digitising:") 
             
             if cont:
-                # add the point to the 3D plot
-                add_point_3d(*position, ax=ax_3d, category=category)
-                
-                self.digitised_points = pd.concat(
-                    [self.digitised_points,
-                     pd.DataFrame.from_dict({
-                         "category": category,
-                         "label": labels[idx-1],
-                         "x": position[0],
-                         "y": position[1],
-                         "z": position[2]
-                     })])
+                self.update_digitised_data(category=category, label=labels[idx - 1], position=position)
+
             else: 
                 # remove the last row of the digitised points
                 self.digitised_points = self.digitised_points.head(-1)
 
-            plt.draw()
-            plt.pause(0.1)
+            self.plotter.refresh_plot()
 
-        plt.close()
-
-
+        self.plotter.close_plot()
 
     def run_digitisation(self):
         """
         
         """
-        fig, ax_3d, ax_text = setup_figure()
 
         for dig in self.digitisation_scheme:
             # to make sure any "residue" button presses from previously does not interfere
@@ -218,10 +233,10 @@ class Digitiser:
             dig_type = dig["dig_type"]
 
             if dig_type == "continuous":
-                self.digitise_continuous(category, dig["n_points"], ax_3d, ax_text)
+                self.digitise_continuous(category, dig["n_points"])
 
             elif dig_type == "single":
-                self.digitise_single(category, labels, ax_3d, ax_text)
+                self.digitise_single(category, labels)
             
             else:
                 raise ValueError(f"dig_type {dig_type} is not implemented. Use either single or continuous")
