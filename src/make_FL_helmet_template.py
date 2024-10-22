0ace4607c9afbefbe9b58145ea756467ee923d69
laurabpaulsen <202005791@post.au.dk>
2024-10-22 15:44:55 +0200

    path relative to file

--- src/make_FL_helmet_template.py
+++ src/make_FL_helmet_template.py
@@ -9,17 +9,19 @@ sys.path.append(str(parent_directory))
 from OPM_lab.sensor_locations import HelmetTemplate
 
 
-data_dict = mat73.loadmat('/Users/au661930/Library/CloudStorage/OneDrive-Aarhusuniversitet/Dokumenter/project placement/OPM_lab/OPM_lab/template/fieldlinealpha1.mat')
-data = data_dict["fieldlinealpha1"]
+if __name__ in "__main__":
+    outpath = Path(__file__).parents[1] / "OPM_lab" / "template"
+    data_dict = mat73.loadmat(outpath / 'fieldlinealpha1.mat')
+    data = data_dict["fieldlinealpha1"]
 
-FL_template = HelmetTemplate(
-    chan_pos=data["chanpos"],
-    chan_ori=data["chanori"],
-    label=[label[0] for label in data["label"]],
-    fid_label=[label[0] for label in data["fid"]["label"]],
-    fid_pos=data["fid"]["pos"],
-    unit=data["unit"])
+    FL_template = HelmetTemplate(
+        chan_pos=data["chanpos"],
+        chan_ori=data["chanori"],
+        label=[label[0] for label in data["label"]],
+        fid_label=[label[0] for label in data["fid"]["label"]],
+        fid_pos=data["fid"]["pos"],
+        unit=data["unit"])
 
-outpath = Path(__file__).parents[1] / "OPM_lab" / "template"
-with open(outpath / "FL_alpha1_helmet.pkl", 'wb') as file:
-    pkl.dump(FL_template, file)
+    outpath = Path(__file__).parents[1] / "OPM_lab" / "template"
+    with open(outpath / "FL_alpha1_helmet.pkl", 'wb') as file:
+        pkl.dump(FL_template, file)
